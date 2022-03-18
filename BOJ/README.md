1dc95631181dcc34820811b0ff083203bb6a047d
김원진 <sobaek2000@naver.com>
2022-03-19 02:25:55 +0900

    pass

--- BOJ/README.md
+++ BOJ/README.md
@@ -66,5 +66,6 @@
 | 1074  |  분할 정복  | [Z](https://github.com/kim-wonjin/Problem-solving/blob/master/BOJ/기타/1074.cpp) |     S1     |  C++   |
 | 1138  |    구현    | [한줄로 서기](https://github.com/kim-wonjin/Problem-solving/blob/master/BOJ/기타/1138.cpp) |     S2     |  C++   |
 | 1181  |  문자열 정렬 | [단어 정렬](https://github.com/kim-wonjin/Problem-solving/blob/master/BOJ/기타/1181.cpp) |     S5     |  C++   |
+| 1753  |  다익스트라 | [최단경로](https://github.com/kim-wonjin/Problem-solving/blob/master/BOJ/기타/1753.cpp) |     G5     |  C++   |
 ></table>
 ></details>
--- BOJ/기타/1753.cpp
+++ BOJ/기타/1753.cpp
@@ -3,29 +3,72 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
-vector <pair<int, int>> adj[300001];
-int min[20001];
+vector <pair<int, int> > adj[300001];
+int min_[20001];
 
-int main ()
+void dijkstra(int start)
+{
+	int cur, cur_cost, i, next, next_cost;
+
+	priority_queue<pair<int, int> > pq;
+	pq.push(make_pair(0, start));
+
+	while (!pq.empty())
+	{
+		cur = pq.top().second;
+		cur_cost = -pq.top().first;
+
+		pq.pop();
+
+		if (min_[cur] < cur_cost)
+			continue;    // 굳이 인접 노드들을 확인 할 필요가 없는 경우
+
+		for (i = 0; i < adj[cur].size(); i++)
+		{
+			next = adj[cur][i].second;
+			next_cost = cur_cost + adj[cur][i].first;
+			if (min_[next] > next_cost)
+			{
+				min_[next] = next_cost;
+				pq.push(make_pair(-next_cost, next));
+			}
+		}
+	}
+}
+
+int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int V, E, K, i, j, a, b;
+	int V, E, K, i;
+	int start, end, cost;
 
 	cin >> V >> E;
 	cin >> K;
-	for (i = 0; i < E; i++)
-		cin >> a >> adj[a].second >> adj[a].first;
-
-	for (i = 0; i < V + 1; i++)
+	for (i = 1; i < E + 1; i++)
 	{
-		for (j = 0; j < V + 1; j++)
-			cout << w[i][j] << ' ';
-		cout << '\n';
+		cin >> start >> end >> cost;
+		adj[start].push_back(make_pair(cost, end));
 	}
+
+	for (i = 1; i < V + 1; i++)
+		min_[i] = INT_MAX;
+
+	min_[K] = 0;
+	dijkstra(K);
+
+	for (i = 1; i < V + 1; i++)
+	{
+		if (min_[i] == INT_MAX)
+			cout << "INF" << '\n';
+		else
+			cout << min_[i] << '\n';
+	}
+	return 0;
 }
